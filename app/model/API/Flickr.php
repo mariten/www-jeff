c517ac59b59482ce5d6ec8c8e15e3ec8421fd4ea
Jeff Case <jeffrey.p.case@gmail.com>
2015-01-23 01:57:12 -0600

    Add common logic for Flickr API calls
    * Add URL builder for Flickr API calls
    * Read API key from secrets constant

---
+++ app/model/API/Flickr.php
@@ -0,0 +1,55 @@
+<?php
+require_once JEFF_BASE_DIR . 'config/secrets/flickr.php';
+require_once JEFF_BASE_DIR . 'app/model/CurlMulti.php';
+
+class API_Flickr
+{
+    const FLICKR_API_URL        = 'https://api.flickr.com/services/rest/';
+    const FLICKR_PUBLIC_PHOTOS  = 1;
+
+    /** Object use to asynchronously call CURL */
+    protected $curl;
+
+    public function __construct()
+    {
+        $this->curl = new CurlMulti();
+    }
+
+
+    //{{{ buildRequestURL(array)
+    /**
+      * Affix global parameters to requests bound for the Flickr API
+      * @param  array    Key-value array with request-specific values already set
+      * @return array    Full URL for this API request, URL-encoded
+      */
+    protected function buildRequestURL($api_params)
+    {
+        // Affix universal parameters
+        $api_params['api_key']          = SECRET_FLICKR_API_KEY;
+        $api_params['privacy_filter']   = self::FLICKR_PUBLIC_PHOTOS;
+        $api_params['format']           = 'json';
+        $api_params['nojsoncallback']   = '1';
+
+        // Build URL
+        $request_params = http_build_query($api_params);
+        $request_url = self::FLICKR_API_URL . '?' . $request_params;
+        return $request_url;
+    }
+    //}}}
+
+
+    //{{{ apiQueryMulti(array)
+    /**
+      * Request multiple URL responses using CURL
+      * @param  array    Key-value array of URL strings (already encoded)
+      * @return array    Key-value array with JSON-decoded response for each URL assigned to the key found in $urls
+      */
+    protected function apiQueryMulti($urls)
+    {
+        $flickr_response_options = array(
+            'decode_json' => true,
+        );
+        return $this->curl->performParallelRequests($urls, $flickr_response_options);
+    }
+    //}}}
+}
---
+++ config/secrets/flickr.php
@@ -0,0 +1,8 @@
+<?php
+/**
+  * Use the following command to not track Git changes to this file:
+  * git update-index --assume-unchanged <file>
+  */
+
+// Flickr API Secrets
+define('SECRET_FLICKR_API_KEY', '<Flickr API Key Here>');
