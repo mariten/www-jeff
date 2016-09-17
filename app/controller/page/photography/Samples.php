7d1d4ce7122cf9ed0e00f3429f76ef1a70c475d3
Jeff Case <jeffrey.p.case@gmail.com>
2016-09-17 12:29:53 -0500

    Use "loadParams" mechanism for Samples

--- app/controller/page/photography/Samples.php
+++ app/controller/page/photography/Samples.php
@@ -3,24 +3,31 @@ require_once JEFF_BASE_DIR . 'app/model/registry/FlickrMariten.php';
 
 class Page_Photography_Samples extends Controller
 {
+    // Constants
     const SAMPLE_SIZE = 6;
+
+    // Request Params
+    protected $album_name = null;
+
+
+    public function loadParams()
+    {
+        $this->album_name = $this->request_params->getParam('album_name', null);
+        if(is_null($this->album_name)) {
+            // If invalid album name requested, re-direct to photography main page
+            $this->doRedirect('/photography');
+        }
+    }
 
 
     public function perform()
     {
-        // Get name of requetsed album name
-        $requested_album = $this->request_params->getParam('album_name');
-        if(is_null($requested_album)) {
-            // If invalid album name requested, re-direct to photography main page
-            $this->doRedirect('/photography');
-        }
-
-        // Select photos and assign to template
-        $sample_photos = $this->selectPhotosRandomlyFromAlbums(array($requested_album), self::SAMPLE_SIZE);
+        $sample_photos = $this->selectPhotosRandomlyFromAlbums(array($this->album_name), self::SAMPLE_SIZE);
         if(!empty($sample_photos)) {
+            // Include album metadata for display
             $album_registry = Registry_FlickrMariten::getAlbums();
-            $this->smarty->assign('album_title', $album_registry[$requested_album]['display']);
-            $this->smarty->assign('album_description', $album_registry[$requested_album]['desc']);
+            $this->smarty->assign('album_title', $album_registry[$this->album_name]['display']);
+            $this->smarty->assign('album_description', $album_registry[$this->album_name]['desc']);
             $this->assignSamplePhotosInRows($sample_photos);
         }
 
